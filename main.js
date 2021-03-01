427671be7d17c1c1b5951ffc4e1cd5dee30b2ee9
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-03-01 12:33:49 -0600

    uwu

--- main.js
+++ main.js
@@ -2,11 +2,43 @@
  * YOUR CODE HERE *
  ******************/
 
+function getFirstItemFrom(array){
+  item = array[0];
+  return item;
+}
 
+function getLastItemFrom(array){
+  item= array.length -1;
+  return array[item];
+}
 
+function getIndex3(array){
+  if(array.length >=4){
+    return array[3];
+  }
+  return array[array.length-1];
+}
 
+function isLongList(array){
+  if(array.length >=10){
+    return true;
+  }else{
+    return false;
+  }
+}
 
+function firstItemIsNumber(array){
+  if(typeof array[0] === "number"){
+    return true;
+  }else{
+    return false;
+  }
+}
 
+function secondCharOfThirdString(array){
+  item =array[2];
+  return item[1];
+}
 
  /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
